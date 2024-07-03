63b5cc7e7d780a961c8040dc90e5b4a177c19cf0
Anikesh7 <kumaranikesh922@gmail.com>
2024-07-03 18:12:36 +0530

    first commit

---
+++ models/curriculo-model.js
@@ -0,0 +1,23 @@
+const data = {
+    title: 'My Resume',
+    name: 'William Oliveira',
+    profession: 'Software Engineer',
+    description: 'Experience in developing single page applications with JavaScript and JavaScript frameworks (I have worked with Angular and React), module bundlers, package managers, transpilers (such as Babel), CSS preprocessors (Sass and Stylus), task managers, CSS architecture (such as BEM and SMACSS), Git, SEO, accessibility, and usability.',
+    experience: [{
+        company: 'Loggi Tecnologia',
+        office: 'Software Engineer',
+        description: 'I work in the development squad of Loggi\'s warehouse management software, ProXD. Our job is to automate the processes of receiving, managing, storing, transferring, and shipping packages, cutting, routing, and monitoring packages and delivery bags for e-commerce.'
+    },
+    {
+        company: 'Dream Company',
+        office: 'Writer',
+        description: 'Author of the book: The Universe of Programming: A Career Guide in Software Development'
+    }],
+    education: [{
+        institution: 'Life',
+        description: 'Survival in the streets'
+    }],
+    skills: ['backend', 'frontend', 'infrastructure', 'mobile']
+}
+
+module.exports = data;
