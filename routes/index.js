b4f97db7a0b2c57a874153df217001806c9ce495
Anikesh7 <kumaranikesh922@gmail.com>
2024-07-03 18:17:20 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.render('index', {
-        title: "Hello from express",
+        title: "Hello from jenkins",
         version: "0.0.0"
     });
 });
