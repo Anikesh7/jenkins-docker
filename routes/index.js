250c61fff39fb3d050ecc32e3464024e0961eec7
Anikesh7 <kumaranikesh922@gmail.com>
2024-07-03 20:36:09 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.render('index', {
         title: "Hello my first successful ci/cd pipeline",
-        version: "0.0.0"
+        version: "0.1.0"
     });
 });
 
